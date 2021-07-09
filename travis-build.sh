03d6b9c8ebc917523880ac45216fc60953b6d2b2
Uri Herrera <hello@uriherrera.com>
2021-07-09 18:46:01 -0500

    build pkg

--- travis-build.sh
+++ travis-build.sh
@@ -5,15 +5,6 @@ set -x
 apt -qq update
 apt -qq -yy install equivs curl git wget gnupg2
 
-### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
-
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	55751E5D > /dev/null
-
-apt -qq update
-
 ### Install Dependencies
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts lintian build-essential automake autotools-dev
 mk-build-deps -i -t "apt-get --yes" -r
